2743b2ae71ed512ee662843ba15b1db503a5807b
Gunn1 <44145072+Gunn1@users.noreply.github.com>
2019-11-21 22:22:33 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const functions = require('./functions.js');
+//This Is For Getting Your Bot Token
 const auth = require('./auth.json');
 const { Client, Attachment } = require('discord.js');
+//translate Might be in a update
 const translate = require('translate-api');
+//This is the prefix
+var prefix = '!';
+//This is the !new Embed
+const newEmbed = new Discord.RichEmbed()
+.setColor('#0099ff')
+.setTitle('UNSC')
+.setURL('https://unscnerf.com')
+.setAuthor('ShadowGun', 'https://i.imgur.com/wSTFkRM.png', 'https://unscnerf.com')
+.setDescription('New')
+.setThumbnail('https://content.shopback.com/sg/wp-content/uploads/2015/05/Neuschwanstein-Castle-Germany.jpg')
+.addField('!admin', 'will request help from a admin')
+.addBlankField()
+.addField('!rip', 'Will display rip', true)
+.addField('!avatar', 'get your avatar', true)
+.addField('!ban (name)', 'This will ban users', true)
+.addField('!purge', 'This Will remove 100 messages from the channel your in unless they are two weeks old', true )
+.addField('!remove (number)', 'This will remove as many messages as you want just enter your number', true)
+.setTimestamp('Thanks for Asking Bot')
+.setFooter('Help Bot', 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1024px-Flag_of_Germany.svg.png');
 
+//When You Start The Bot it Prints this to the console 
 client.on('ready', () => {
+    //Prints Logged in as Bot Name to the console
     console.log(`Logged in as ${client.user.tag}!`);
 });
 client.on('message', msg => {
 	if (msg.content == "!new") {
 		msg.delete()
-    	const newEmbed = new Discord.RichEmbed()
-			.setColor('#0099ff')
-			.setTitle('UNSC')
-			.setURL('https://unscnerf.com')
-			.setAuthor('ShadowGun', 'https://i.imgur.com/wSTFkRM.png', 'https://unscnerf.com')
-			.setDescription('New')
-			.setThumbnail('https://content.shopback.com/sg/wp-content/uploads/2015/05/Neuschwanstein-Castle-Germany.jpg')
-			.addField('!admin', 'will request help from a admin')
-			.addBlankField()
-			.addField('!rip', 'Will display rip', true)
-			.addField('!avatar', 'get your avatar', true)
-			.addField('!ban (name)', 'This will ban users', true)
-			.addField('!purge', 'This Will remove 100 messages from the channel your in unless they are two weeks old', true )
-			.addField('!remove (number)', 'This will remove as many messages as you want just enter your number', true)
-			.setTimestamp('Thanks for Asking Bot')
-			.setFooter('Help Bot', 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1024px-Flag_of_Germany.svg.png');
-
 	msg.channel.send(newEmbed);
 	}
 
-
-	  // if the message content starts with "!ban"
+//This Will Ban users
+// if the message content starts with "!ban"
   if (msg.content.startsWith('!ban')) {
   	if (msg.member.roles.find(r => r.name === "Admin") || msg.member.roles.find(r => r.name === "Owner")) {
 
@@ -75,99 +82,33 @@ client.on('message', msg => {
     }
 }
   }
-
-
-  	if (msg.content === 'ping') {
-    	msg.reply('pong');
-    	pms();
-  	}
-  	if (msg.content == "!Do") {
-  		msg.reply("I can do lots of things like remove messages and getting your avatar I also can help you! just Private Message Me !admin for help")
-  	}
-  	var prefix = '!';
-  	/*if (msg.content.startsWith(prefix))*/ //{
 		var message = msg.content;
     	message = message.slice(1);
     	var num = msg.content.replace( /^\D+/g, '');
     	if (msg.content == prefix + 'purge') {
-    		async function purge() {
+        //Deletes 100 messages
+         async function purge() {
+            if (msg.member.roles.find(r => r.name === "Admin") || msg.member.roles.find(r => r.name === "Owner")) {
             msg.delete();
             const fetched = await msg.channel.fetchMessages({limit: 100});
             msg.channel.bulkDelete(fetched);
+     
         }
-        if (msg.member.roles.find(r => r.name === "Admin") || msg.member.roles.find(r => r.name === "Owner")) {
-        purge()
+        purge();
     }
-    	}
     	if (msg.content == prefix + "remove " + num) {
-
-			async function clear() {
+        async function clear() {
+        if (msg.member.roles.find(r => r.name === "Admin") || msg.member.roles.find(r => r.name === "Owner")) { 
             msg.delete();
             const fetched = await msg.channel.fetchMessages({limit: num});
             msg.channel.bulkDelete(fetched);
             client.channels.get("641442259417563138").send(num + " Messages Have Been Deleted By " + msg.author)
-
         }
-if (msg.member.roles.find(r => r.name === "Admin") || msg.member.roles.find(r => r.name === "Owner")) {
-        clear();
     }
-    	}
-    	if (message === 'rip') {
-        // Create the attachment using Attachment
-        const attachment = new Attachment('https://i.imgur.com/w3duR07.png');
-        // Send the attachment in the message channel
-        msg.channel.send(attachment);
+    clear();
     }
-
-    	if (message == 'avatar') {
-			msg.reply('Getting your avatar ' + msg.author)
-    		msg.reply(msg.author.avatarURL);
-    	}
-    	if (message == 'admin') {
-    		msg.author.send("Sorry Was not able to start please say !admin again")
-    		msg.author.send("What do you want the message to include?:")
-        const collector = new Discord.MessageCollector(msg.channel, m => m.author.id === msg.author.id, {max: 1});
-        console.log(collector)
-        collector.on('collect', msg => {
-        	usermessage = msg.content;
-            if (usermessage == " ") {
-            	msg.reply("Please Enter Text");
-            }
-            if (msg.content == "!ban") {
-            	msg.reply("Banning Admin")
-            }
-
-
-            if ( usermessage != " " ){
-            msg.author.send("Thanks Sending it now");
-		    client.channels.get("641442259417563138").send(msg.author + " @here Needs Help With: " + usermessage )
-            }
-        })
-    	
-    }
-    	
-    	if (message == 'help') {
-    		msg.delete()
-    	const exampleEmbed = new Discord.RichEmbed()
-			.setColor('#0099ff')
-			.setTitle('UNSC')
-			.setURL('https://unscnerf.com')
-			.setAuthor('ShadowGun', 'https://i.imgur.com/wSTFkRM.png', 'https://unscnerf.com')
-			.setDescription('Help')
-			.setThumbnail('https://content.shopback.com/sg/wp-content/uploads/2015/05/Neuschwanstein-Castle-Germany.jpg')
-			.addField('!admin', 'will request help from a admin')
-			.addBlankField()
-			.addField('!rip', 'Try it and find out', true)
-			.addField('!avatar', 'get your avatar', true)
-			.setTimestamp('Thanks for Asking Bot')
-			.setFooter('Help Bot', 'https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1024px-Flag_of_Germany.svg.png');
-
-	msg.channel.send(exampleEmbed);    	
-
-
-  	
 }
-//}
+
 });
 
 
